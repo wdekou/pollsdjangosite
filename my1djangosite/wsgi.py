32f0309e96be480e292ccb9685237f0b1f60daa3
William K. DEKOU <williamdekou@gmail.com>
2018-10-02 00:30:11 +0100

    renaming the project to pollsdjangosite

--- my1djangosite/wsgi.py
+++ my1djangosite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for my1djangosite project.
+WSGI config for pollsdjangosite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my1djangosite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollsdjangosite.settings')
 
 application = get_wsgi_application()
