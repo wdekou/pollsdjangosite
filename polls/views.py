d15a0666e40be463a070ee2be2c72a129737f90e
William K. DEKOU <williamdekou@gmail.com>
2018-10-02 13:10:15 +0100

    working with test

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
+
 from django.db.models import F
 from django.views import generic
 from .models import Question, Choice
@@ -19,6 +21,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
