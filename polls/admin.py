c817727bb69ed8650225d60633546d28da68e3a6
William K. DEKOU <williamdekou@gmail.com>
2018-10-02 11:41:03 +0100

    Refactor views

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Question, Choice
 # Register your models here.
 admin.site.register(Question)
+admin.site.register(Choice)
