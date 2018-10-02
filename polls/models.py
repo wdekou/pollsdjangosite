e0113ed5547d41c2ac275a5e485a987ad9cf3639
William K. DEKOU <williamdekou@gmail.com>
2018-10-02 01:31:05 +0100

    Add models Choice and  migrate data

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ class Question(models.Model):
   quest_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
 
-class Choise(models.Model):
+class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
   choice_text = models.CharField(max_length=200)
   vote = models.IntegerField(default=0)
