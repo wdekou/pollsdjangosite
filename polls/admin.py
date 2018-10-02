4ee97b2986a1e3c6b6841d5d21d02d1d3cdefffa
William K. DEKOU <williamdekou@gmail.com>
2018-10-02 13:53:50 +0100

    costomize the admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
